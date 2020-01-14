72c1d22c5df75b5d2893a4ebfa751759da991975
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-14 11:35:57 -0800

    Feature Task #2: Pre-Deployment

--- server.js
+++ server.js
@@ -9,14 +9,28 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Use CORS to allow our server to pass data to the front-end.
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send(
-    'City Explorer is not ready yet. We are working on it though, so check back soon!'
-  );
+// Define routes.
+app.get('/location', (req, res) => {
+  const geoDataArray = require('./data/geo.json');
+  const geoData = geoDataArray[0];
+  let city = req.query.city;
+
+  let location = new Location(city, geoData);
+  res.send(location);
 });
 
+// Define functions.
+function Location(city, localData) {
+  this.search_query = city;
+  this.formatted_query = localData.display_name;
+  this.latitude = localData.lat;
+  this.longitude = localData.lon;
+}
+
+// Tell our server to listen on port variable PORT.
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
