06483bcff3e158da66a4f69eabdcf4ba8f9e1d5f
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-16 10:44:19 -0800

    Updated connection to database.

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS cities;
 CREATE TABLE cities
 (
   id SERIAL PRIMARY KEY,
-  city_name VARCHAR(255),
-  formatted_name VARCHAR(255),
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
   latitude VARCHAR(255),
   longitude VARCHAR(255)
 );
